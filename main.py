5c2657e16ffe4a76cbcc8f09f9922578d853d29d
mightcontainnuts <mightcontainnuts@icloud>
2025-03-29 14:34:49 +0100

    set up file structure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from surestride-portfolio!")
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return JSONResponse(content={"message": "Hello World"})
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
