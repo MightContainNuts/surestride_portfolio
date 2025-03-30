56caa2c1db78d9da816f34266ca681a7e788d7b8
mightcontainnuts <mightcontainnuts@icloud>
2025-03-30 16:35:35 +0200

    initial pages - and some baic css

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/")
-def read_root():
-    return JSONResponse(content={"message": "Hello World"})
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # You can pass variables to your template here
+    return templates.TemplateResponse("index.html", {"request": request, "name": "FastAPI"})
+
+@app.get("/about", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # You can pass variables to your template here
+    return templates.TemplateResponse("about.html", {"request": request, "name": "FastAPI"})
+
+@app.get("/certificates", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # You can pass variables to your template here
+    return templates.TemplateResponse("certificates.html", {"request": request, "name": "FastAPI"})
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
+
