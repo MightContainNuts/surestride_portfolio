2610fb14ace2d8694f8d86119d430b61344d2831
mightcontainnuts <mightcontainnuts@icloud>
2025-03-31 14:47:50 +0200

    added basic page

--- main.py
+++ main.py
@@ -10,19 +10,27 @@ templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    # You can pass variables to your template here
-    return templates.TemplateResponse("index.html", {"request": request, "name": "FastAPI"})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/about", response_class=HTMLResponse)
 async def read_root(request: Request):
-    # You can pass variables to your template here
-    return templates.TemplateResponse("about.html", {"request": request, "name": "FastAPI"})
+    return templates.TemplateResponse("about.html", {"request": request})
 
 @app.get("/certificates", response_class=HTMLResponse)
 async def read_root(request: Request):
-    # You can pass variables to your template here
-    return templates.TemplateResponse("certificates.html", {"request": request, "name": "FastAPI"})
+    return templates.TemplateResponse("certificates.html", {"request": request})
 
+@app.get("/web_tech_stack", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("web_tech_stack.html", {"request": request})
+
+@app.get("/chat", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
+
+@app.get("/legal_stuff", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("legal_stuff.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
