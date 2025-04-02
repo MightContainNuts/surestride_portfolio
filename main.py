da3a195837206e7bc9d7b60d414ac04ac40f8110
mightcontainnuts <mightcontainnuts@icloud>
2025-04-02 12:18:36 +0200

    added chat window

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
+from pydantic import BaseModel
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
+
+messages = []
+user_handlers = {}
+
+
+class Message(BaseModel):
+    sender: str
+    message: str
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -31,6 +41,29 @@ async def read_root(request: Request):
 @app.get("/legal_stuff", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("legal_stuff.html", {"request": request})
+
+
+@app.post("/chat/send_message")
+def send_message(user_msg: Message, request: Request):
+    if not user_msg.message.strip():
+        raise HTTPException(status_code=400, detail="Message cannot be empty")
+
+    print(f"✌🏻 Message received: {user_msg.message}")
+    messages.append(user_msg)
+
+    ai_msg = Message(sender="🤖", message="This is a dummy response from the AI. #TODO ")
+
+    print(f"📩 {ai_msg}")
+    messages.append(ai_msg)
+
+    # Return status and updated messages
+    return {"status": "success", "messages": messages}
+
+
+@app.get("/chat/get_messages", response_class=JSONResponse)
+def get_messages():
+    return {"messages": messages}
+
 
 if __name__ == "__main__":
     import uvicorn
